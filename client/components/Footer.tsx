9aa3645d3c91ac88e11358a2f11a1dfbe327df70
amenline <amenline.07@gmail.com>
2021-08-09 20:27:26 +0000

    completed the footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -7,8 +7,9 @@ const Footer: React.FC = () => {
   return (
     <footer className={`${styles.grayish} text-white text-fonts`}>
       <div className='container m-auto py-4 flex justify-evenly'>
-        <section>
-          <h2 className='uppercase font-bold mb-3'>Quick Links</h2>
+        <section className='hidden md:block'>
+          {/* <h2 className='uppercase font-bold mb-3'></h2> */}
+          <Image src={logo} alt='AkuafuLink logo' />
           <nav className={`${styles.nav} text-sm`}>
             <Link href='#'>
               <a
@@ -18,7 +19,7 @@ const Footer: React.FC = () => {
               >
                 <img
                   className='pr-2 inline-block'
-                  src='/images/facebook.svg'
+                  src='/icons/facebook.svg'
                   alt='Facebook'
                 />
                 Facebook
@@ -32,7 +33,7 @@ const Footer: React.FC = () => {
               >
                 <img
                   className='pr-2 inline-block'
-                  src='/images/twitter.svg'
+                  src='/icons/twitter.svg'
                   alt='Twitter'
                 />
                 Twitter
@@ -46,7 +47,7 @@ const Footer: React.FC = () => {
               >
                 <img
                   className='pr-2 inline-block'
-                  src='/images/instagram.svg'
+                  src='/icons/instagram.svg'
                   alt='Instagram'
                 />
                 Instagram
@@ -55,7 +56,9 @@ const Footer: React.FC = () => {
           </nav>
         </section>
         <section>
-          <h2 className='uppercase font-bold mb-3'>Quick Links</h2>
+          <h2 className={`uppercase font-bold mb-3 ${styles.heading}`}>
+            Quick Links
+          </h2>
           <nav className={`${styles.nav} text-sm`}>
             <Link href='#'>
               <a className='block py-1 hover:text-primary-400'>Wholesale</a>
@@ -71,8 +74,10 @@ const Footer: React.FC = () => {
             </Link>
           </nav>
         </section>
-        <section className='hidden md:block'>
-          <h2 className='uppercase font-bold mb-3'>Top Selling</h2>
+        <section>
+          <h2 className={`uppercase font-bold mb-3 ${styles.heading}`}>
+            Top Selling
+          </h2>
           <nav className={`${styles.nav} text-sm`}>
             <Link href='/'>
               <a className='block py-1 hover:text-primary-400'>Vegetables</a>
@@ -86,39 +91,30 @@ const Footer: React.FC = () => {
           </nav>
         </section>
       </div>
-      <section className='text-center py-4'>
-        <nav className='flex justify-center'>
+      <section className={`${styles.nav} text-center py-4`}>
+        <nav className='flex justify-center md:hidden'>
           <Link href='#'>
             <a className='p-2 m-1' target='_blank' rel='noopener noreferrer'>
-              <img src='/images/youtube.svg' alt='Youtube' />
+              <img src='/icons/facebook.svg' alt='Facebook' />
             </a>
           </Link>
           <Link href='#'>
             <a className='p-2 m-1' target='_blank' rel='noopener noreferrer'>
-              <img src='/images/facebook.svg' alt='Facebook' />
+              <img src='/icons/instagram.svg' alt='Instagram' />
             </a>
           </Link>
           <Link href='#'>
             <a className='p-2 m-1' target='_blank' rel='noopener noreferrer'>
-              <img src='/images/instagram.svg' alt='Instagram' />
-            </a>
-          </Link>
-          <Link href='#'>
-            <a className='p-2 m-1' target='_blank' rel='noopener noreferrer'>
-              <img src='/images/twitter.svg' alt='Twitter' />
+              <img src='/icons/twitter.svg' alt='Twitter' />
             </a>
           </Link>
         </nav>
-        {/* <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='p-1'
-        >
-          Powered by AKUAFULINK
-        </a> */}
         <p className='text-sm p-1'>
-          &copy; {new Date().getFullYear()} AkuafuLink. All rights reserved.
+          &copy; {new Date().getFullYear()}{' '}
+          <Link href='/'>
+            <a className='hover:text-primary-400'>AkuafuLink.</a>
+          </Link>{' '}
+          All rights reserved.
         </p>
       </section>
     </footer>
